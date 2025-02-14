b1849d976809e6511653f68d9270ef57e9871b34
Erich124 <ehu6688@gmail.com>
2025-02-14 15:21:01 -0800

    Eric Hu's CSA homework Github 4 - 2D Arrays

--- Array.java
+++ Array.java
@@ -7,7 +7,14 @@ public class Array
 	// 7 8 9
 	public int[][] getRowMajorSquare(int x)
 	{
-//	
+        int[][] array = new int[x][x];
+        int value = 1;
+        for (int i = 0; i < x; i++) {
+            for(int j = 0; j < x; j++) {
+                array[i][j] = value++;
+            }
+        }
+        return array;
 	}
 
 	// Array should be of format
@@ -16,7 +23,14 @@ public class Array
 	// 3 6 9
 	public int[][] getColumnMajorSquare(int x)
 	{
-//		
+    	int[][] array = new int[x][x];
+        int value = 1;
+        for (int j = 0; j < x; j++) {
+            for (int i = 0; i < x; i++) {
+                array[i][j] = value++;
+            }
+        }
+        return array;
 	}
 
 	// Array should be of format
@@ -25,7 +39,14 @@ public class Array
 	// 9 10 11 12
 	public int[][] getRowMajorRectangle(int row, int column)
 	{
-		
+		int[][] array = new int[row][column];
+        int value = 1;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                array[i][j] = value++;
+            }
+        }
+        return array;
 	}
 
 	// Array should be of format
@@ -34,7 +55,14 @@ public class Array
 	// 3 6 9 12
 	public int[][] getColumnMajorRectangle(int column, int row)
 	{
-	
+	int[][] array = new int[row][column];
+        int value = 1;
+        for (int j = 0; j < column; j++) {
+            for (int i = 0; i < row; i++) {
+                array[i][j] = value++;
+            }
+        }
+        return array;
 	}
 
 	// Please note that each digit occupies two spaces and there is a space between
@@ -42,7 +70,11 @@ public class Array
 	// You are not required to deal with three digit numbers.
 	public void printDouble(int[][] a)
     {
-	
-
+	for (int[] row : a) {
+            for (int num : row) {
+                System.out.printf("%2d ", num);
+            }
+            System.out.println();
+        }
 	}
 }
